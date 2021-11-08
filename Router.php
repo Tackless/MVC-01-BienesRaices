2c684f1e138228f85fe7a5b4a2036417ee0dd213
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-08 13:21:56 -0600

    register url's

--- Router.php
+++ Router.php
@@ -3,7 +3,28 @@
 namespace MVC;
 
 class Router {
-    function __construct() {
-        echo "Creando el Router...";
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+    
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/admin';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) {
+            
+            call_user_func($fn, $this);
+        } else {
+            echo 'Page not found';
+        }
+
     }
 }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/tienda', 'funcion_tienda');
+$router->get('/contacto', 'funcion_contacto');
+$router->get('/admin', 'funcion_admin');
+
+$router->comprobarRutas();
