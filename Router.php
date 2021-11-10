d25bb6841b1d88d95537584a2e2a54c1fcd2ad48
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-09 22:51:12 -0600

    add URL's

--- Router.php
+++ Router.php
@@ -16,7 +16,7 @@ class Router {
     }
 
     public function comprobarRutas() {
-        $urlActual = $_SERVER['PATH_INFO'] ?? '/admin';
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
         if ($metodo == 'GET') {
---
+++ controllers/PaginasController.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Controllers;
+
+class PaginasController {
+    public static function index() {
+        debuguear('Desde index');
+    }
+    
+    public static function nosotros() {
+        debuguear('Desde nosotros');
+    }
+    
+    public static function propiedades() {
+        debuguear('Desde propiedades');
+    }
+    
+    public static function propiedad() {
+        debuguear('Desde propiedad');
+    }
+    
+    public static function blog() {
+        debuguear('Desde blog');
+    }
+    
+    public static function entrada() {
+        debuguear('Desde entrada');
+    }
+    
+    public static function contacto() {
+        debuguear('Desde contacto');
+    }
+    
+}
